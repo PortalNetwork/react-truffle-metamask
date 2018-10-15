0f1c4cd887a7598a8410bf261159e0b602406230
Jacky <jacky@Jackyde-MacBook-Pro.local>
2018-10-15 17:29:17 +0800

    fix MetaMask components

--- src/components/MetaMask/MetaMask.js
+++ src/components/MetaMask/MetaMask.js
@@ -1,12 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Web3 from 'web3';
 import Button from '@material-ui/core/Button';
-import Dialog, {
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-} from '@material-ui/core/Dialog';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import Slide from '@material-ui/core/Slide';
 
 const messages = {
@@ -21,7 +20,7 @@ const MetaMaskInstallDialog = (props) => (
   <Dialog
     className="MetaMaskDialog"
     open={props.metaMaskInstallDialogOpen}
-    transition={Slide}>
+    TransitionComponent={Slide}>
     <DialogTitle>{"Oops, you haven't install MetaMask"}</DialogTitle>
     <DialogContent>
       <DialogContentText>
@@ -29,10 +28,10 @@ const MetaMaskInstallDialog = (props) => (
       </DialogContentText>
     </DialogContent>
     <DialogActions>
-      <Button raised href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?utm_source=chrome-ntp-icon" color="primary">
+      <Button variant="text" href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?utm_source=chrome-ntp-icon" color="primary">
         Install MetaMask
       </Button>
-      <Button raised onClick={props.handleMetaMaskInstallDialogClose} color="primary">
+      <Button variant="text" onClick={props.handleMetaMaskInstallDialogClose} color="primary">
         I understand, continue
       </Button>
     </DialogActions>
@@ -43,7 +42,7 @@ const MetaMaskLockDialog = (props) => (
   <Dialog
     className="MetaMaskDialog"
     open={props.metaMaskLockDialogOpen}
-    transition={Slide}>
+    TransitionComponent={Slide}>
     <DialogTitle>{"Oops, your MetaMask is locked"}</DialogTitle>
     <DialogContent>
       <DialogContentText>
@@ -51,7 +50,7 @@ const MetaMaskLockDialog = (props) => (
       </DialogContentText>
     </DialogContent>
     <DialogActions>
-      <Button raised onClick={props.handleMetaMaskLockDialogClose} color="primary">
+      <Button variant="text" onClick={props.handleMetaMaskLockDialogClose} color="primary">
         I understand, continue
       </Button>
     </DialogActions>
@@ -75,7 +74,7 @@ export class MetaMask extends Component {
     let web3 = window.web3;
     if (typeof web3 === 'undefined') {
       this.props.setWeb3(null);
-      this.setState({message: messages.METAMASK_NOT_INSTALL});
+      this.setState({ message: messages.METAMASK_NOT_INSTALL });
     }
   }
 
@@ -84,11 +83,11 @@ export class MetaMask extends Component {
     if (this.props.web3 !== null) {
       this.props.web3.eth.getAccounts((err, accounts) => {
         if (err) {
-          this.setState({message: messages.LOAD_MATAMASK_WALLET_ERROR});
+          this.setState({ message: messages.LOAD_MATAMASK_WALLET_ERROR });
         } else {
           if (accounts.length === 0) {
             this.props.handleMetaMaskAccount(null);
-            this.setState({metaMaskLockDialogOpen: true, message: messages.EMPTY_METAMASK_ACCOUNT})
+            this.setState({ metaMaskLockDialogOpen: true, message: messages.EMPTY_METAMASK_ACCOUNT })
           } else {
             // if account changed then change redux state
             if (accounts[0] !== this.props.metaMask.account) {
@@ -106,7 +105,7 @@ export class MetaMask extends Component {
       this.props.web3.version.getNetwork((err, netId) => {
         if (err) {
           this.props.handleMetaMaskNetwork(null);
-          this.setState({metaMaskLockDialogOpen: true, message: messages.NETWORK_ERROR });
+          this.setState({ metaMaskLockDialogOpen: true, message: messages.NETWORK_ERROR });
         } else {
           // if network changed then change redux state
           if (netId !== this.props.metaMask.network) {
@@ -119,7 +118,7 @@ export class MetaMask extends Component {
 
   componentDidMount() {
     let self = this;
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
       let web3 = window.web3;
       if (typeof web3 !== 'undefined') {
         window.web3 = new Web3(web3.currentProvider);
@@ -128,9 +127,9 @@ export class MetaMask extends Component {
         self.fetchNetwork();
         self.Web3Interval = setInterval(() => self.fetchWeb3(), 1000);
         self.AccountInterval = setInterval(() => self.fetchAccounts(), 1000);
-        self.NetworkInterval = setInterval(() => self.fetchNetwork(),  1000);
+        self.NetworkInterval = setInterval(() => self.fetchNetwork(), 1000);
       } else {
-        self.setState({metaMaskInstallDialogOpen: true, message: messages.METAMASK_NOT_INSTALL});
+        self.setState({ metaMaskInstallDialogOpen: true, message: messages.METAMASK_NOT_INSTALL });
       }
     })
   }
@@ -142,16 +141,16 @@ export class MetaMask extends Component {
   }
 
   handleMetaMaskInstallDialogClose() {
-    this.setState({metaMaskInstallDialogOpen: false, disableDialog: true});
+    this.setState({ metaMaskInstallDialogOpen: false, disableDialog: true });
   }
 
   handleMetaMaskLockDialogClose() {
-    this.setState({metaMaskLockDialogOpen: false, disableDialog: true});
+    this.setState({ metaMaskLockDialogOpen: false, disableDialog: true });
   }
 
   render() {
     const metaMaskInstall = this.state.disableDialog === false &&
-      <MetaMaskInstallDialog 
+      <MetaMaskInstallDialog
         {...this.state}
         handleMetaMaskInstallDialogClose={this.handleMetaMaskInstallDialogClose}
       />;
